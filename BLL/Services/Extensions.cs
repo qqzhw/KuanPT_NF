77ea14db6d1af02be4ac1aa62e1923b9532fc2a8
zhw <zhanghw@pvirtech.com>
2017-06-07 11:51:07 +0800

    图片类及接口添加

---
+++ BLL/Services/Extensions.cs
@@ -0,0 +1,71 @@
+using BLL.Infrastructure;
+using BLL.Services;
+using Model;
+using System.IO;
+using System.Web;
+namespace  BLL
+{
+    /// <summary>
+    /// Extensions
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Gets the download binary array
+        /// </summary>
+        /// <param name="postedFile">Posted file</param>
+        /// <returns>Download binary array</returns>
+        public static byte[] GetDownloadBits(this HttpPostedFile postedFile)
+        {
+            Stream fs = postedFile.InputStream;
+            int size = postedFile.ContentLength;
+            byte[] binary = new byte[size];
+            fs.Read(binary, 0, size);
+            return binary;
+        }
+
+        /// <summary>
+        /// Gets the picture binary array
+        /// </summary>
+        /// <param name="postedFile">Posted file</param>
+        /// <returns>Picture binary array</returns>
+        public static byte[] GetPictureBits(this HttpPostedFile postedFile)
+        {
+            Stream fs = postedFile.InputStream;
+            int size = postedFile.ContentLength;
+            byte[] img = new byte[size];
+            fs.Read(img, 0, size);
+            return img;
+        }
+
+        /// <summary>
+        /// Gets the loaded picture binary depending on picture storage settings
+        /// </summary>
+        /// <param name="picture">Picture</param>
+        /// <param name="fromDb">Load from database; otherwise, from file system</param>
+        /// <returns>Picture binary</returns>
+        public static byte[] LoadPictureBinary(this Picture picture, bool fromDb)
+        {
+            byte[] result = null;
+            if (fromDb)
+            {
+                result = picture.PictureBinary;
+            }
+            else
+            {
+                result =EngineContext.Current.Resolve<IPictureService>().LoadPictureFromFile(picture.PictureId, picture.MimeType);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the loaded picture binary depending on picture storage settings
+        /// </summary>
+        /// <param name="picture">Picture</param>
+        /// <returns>Picture binary</returns>
+        public static byte[] LoadPictureBinary(this Picture picture)
+        {
+            return LoadPictureBinary(picture, EngineContext.Current.Resolve<IPictureService>().StoreInDB);
+        }
+    }
+}
---
+++ Model/Picture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Picture
+    {
+        
+        public int PictureId { get; set; }
+
+       
+        public byte[] PictureBinary { get; set; } 
+     
+        public string MimeType { get; set; }
+
+        
+        public bool IsNew { get; set; }
+
+    
+    }
+}
